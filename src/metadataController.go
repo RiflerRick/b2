d75487404939912f8c66f5b0b99a6e9910e19286
RiflerRick <rajdeep.mukherjee@bigbasket.com>
2019-05-17 19:00:24 +0530

    all mutex unlocks deferred

--- src/metadataController.go
+++ src/metadataController.go
@@ -12,18 +12,12 @@ func (m DesiredMetadata) read(queryType *string, typeOfData *string, data *inter
 		mutex = dmCPMMutex[*queryType]
 		glog.V(4).Infof("Acquiring read lock for cpm for queryType: %s", *queryType)
 		mutex.RLock()
+		defer mutex.RUnlock()
 		d := m.cpm[*queryType].(interface{})
-		*data = d
-	} else if *typeOfData == "wT" {
-		mutex = dmWTMutex[*queryType]
-		glog.V(4).Infof("Acquiring read lock for wT for queryType: %s", *queryType)
-		mutex.RLock()
-		d := m.wT[*queryType].(interface{})
 		*data = d
 	}
 	glog.V(4).Infof("Releasing read lock for %s for queryType: %s", *typeOfData, *queryType)
-	mutex.RUnlock()
-	// TODO: use of RLock needs to be revisited. The difference between Lock and RLock needs to be clearly understood
+
 }
 
 func (m DesiredMetadata) write(queryType *string, typeOfData *string, data interface{}) {
@@ -32,52 +26,10 @@ func (m DesiredMetadata) write(queryType *string, typeOfData *string, data inter
 		mutex = dmCPMMutex[*queryType]
 		glog.V(4).Infof("Acquiring lock for cpm for queryType: %s", *queryType)
 		mutex.Lock()
+		defer mutex.Unlock()
 		m.cpm[*queryType] = data
-	} else if *typeOfData == "wT" {
-		mutex = dmWTMutex[*queryType]
-		glog.V(4).Infof("Acquiring lock for wT for queryType: %s", *queryType)
-		mutex.Lock()
-		m.wT[*queryType] = data
 	}
 	glog.V(4).Infof("Releasing lock for %s for queryType: %s", *typeOfData, *queryType)
-	mutex.Unlock()
-}
-
-func (m RunMetadata) read(queryType *string, typeOfData *string, data *interface{}) {
-	var mutex *sync.RWMutex
-	if *typeOfData == "cpm" {
-		mutex = rmCPMMutex[*queryType]
-		glog.V(4).Infof("Acquiring read lock for cpm for queryType: %s", *queryType)
-		mutex.RLock()
-		d := m.cpm[*queryType].(interface{})
-		*data = d
-	} else if *typeOfData == "wT" {
-		mutex = rmWTMutex[*queryType]
-		glog.V(4).Infof("Acquiring read lock for wT for queryType: %s", *queryType)
-		mutex.RLock()
-		d := m.wT[*queryType].(interface{})
-		*data = d
-	}
-	glog.V(4).Infof("Releasing read lock for %s for queryType: %s", *typeOfData, *queryType)
-	mutex.RUnlock()
-	// TODO: use of RLock needs to be revisited. The difference between Lock and RLock needs to be clearly understood
-}
-
-func (m RunMetadata) write(queryType *string, typeOfData *string, data interface{}) {
-	var mutex *sync.RWMutex
-	if *typeOfData == "cpm" {
-		mutex = rmCPMMutex[*queryType]
-		glog.V(4).Infof("Acquiring lock for cpm for queryType: %s", *queryType)
-		mutex.Lock()
-		m.cpm[*queryType] = data
-	} else if *typeOfData == "wT" {
-		mutex = rmWTMutex[*queryType]
-		glog.V(4).Infof("Acquiring lock for wT for queryType: %s", *queryType)
-		mutex.Lock()
-		m.wT[*queryType] = data
-	}
-	glog.V(4).Infof("Releasing lock for %s for queryType: %s", *typeOfData, *queryType)
-	mutex.Unlock()
 }
 
 func (m ControllerMetadata) read(queryType *string, typeOfData *string, data *interface{}) {
@@ -90,6 +42,7 @@ func (m ControllerMetadata) read(queryType *string, typeOfData *string, data *in
 		}
 		glog.V(4).Infof("Acquiring read lock for %s instances for queryType: %s", m.controllerType, *queryType)
 		mutex.RLock()
+		defer mutex.RUnlock()
 		d := m.instances[*queryType].(interface{})
 		*data = d
 	} else if *typeOfData == "chunk_size" {
@@ -100,6 +53,7 @@ func (m ControllerMetadata) read(queryType *string, typeOfData *string, data *in
 		}
 		glog.V(4).Infof("Acquiring read lock for %s chunk_size for queryType: %s", m.controllerType, *queryType)
 		mutex.RLock()
+		defer mutex.RUnlock()
 		d := m.chunkSize[*queryType].(interface{})
 		*data = d
 	} else if *typeOfData == "sleep_time" {
@@ -110,12 +64,11 @@ func (m ControllerMetadata) read(queryType *string, typeOfData *string, data *in
 		}
 		glog.V(4).Infof("Acquiring read lock for %s sleep_time for queryType: %s", m.controllerType, *queryType)
 		mutex.RLock()
+		defer mutex.RUnlock()
 		d := m.sleepTime[*queryType].(interface{})
 		*data = d
 	}
-
 	glog.V(4).Infof("Releasing read lock for %s %s for queryType: %s", m.controllerType, *typeOfData, *queryType)
-	mutex.RUnlock()
 	// TODO: use of RLock needs to be revisited. The difference between Lock and RLock needs to be clearly understood
 }
 
@@ -129,6 +82,7 @@ func (m ControllerMetadata) write(queryType *string, typeOfData *string, data in
 		}
 		glog.V(4).Infof("Acquiring lock for %s instances for queryType: %s", m.controllerType, *queryType)
 		mutex.Lock()
+		defer mutex.Unlock()
 		m.instances[*queryType] = data
 	} else if *typeOfData == "chunk_size" {
 		if m.controllerType == "publish" {
@@ -138,6 +92,7 @@ func (m ControllerMetadata) write(queryType *string, typeOfData *string, data in
 		}
 		glog.V(4).Infof("Acquiring lock for %s chunk_size for queryType: %s", m.controllerType, *queryType)
 		mutex.Lock()
+		defer mutex.Unlock()
 		m.chunkSize[*queryType] = data
 	} else if *typeOfData == "sleep_time" {
 		if m.controllerType == "publish" {
@@ -147,8 +102,8 @@ func (m ControllerMetadata) write(queryType *string, typeOfData *string, data in
 		}
 		glog.V(4).Infof("Acquiring lock for %s sleep_time for queryType: %s", m.controllerType, *queryType)
 		mutex.Lock()
+		defer mutex.Unlock()
 		m.sleepTime[*queryType] = data
 	}
 	glog.V(4).Infof("Releasing lock for %s %s for queryType: %s", m.controllerType, *typeOfData, *queryType)
-	mutex.Unlock()
 }
